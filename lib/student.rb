c8e58cbfc148ddec343430274a9d44f96ae2cdd7
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-19 20:58:21 -0400

    notes on the original and the official solutions

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,28 @@ class Student
     @id = id
   end
 
+  # This original method passes the tests.  It may be preferable to the
+  #  official solution if it is desired that the "id" be passed to the 
+  #  INSERT INTO command.
+  def save_0
+    sql = <<-SQL
+    INSERT INTO students (
+      id, name, grade)
+    VALUES (
+      ?, ?, ?
+    )
+    SQL
+    DB[:conn].execute(sql, 
+      self.id, self.name, self.grade
+    )
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    self
+  end
+
+  # This slightly modified method from the official solution passes
+  #  the tests as well, but may be preferable if it is not desired to
+  #  pass the "id" into the INSERT INTO command. 
   def save
     sql = <<-SQL
     INSERT INTO students (
